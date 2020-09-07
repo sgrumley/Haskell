c5caa38f0b8d5a54a40d895d8174fff6dd0acde1
Sam Grumley <grumsamu@gmail.com>
2020-09-07 01:40:16 -0700

    Simpsons working

--- simpson/Simpson.hs
+++ simpson/Simpson.hs
@@ -1,13 +1,75 @@
 
 module Simpson(
-    integrate
+    integrate,
+    points,
+    driver
 ) where
    
 -- Name general functions as Func
 type Func = Double -> Double
 
-integrate:: Func -> Double -> Double -> Int -> Double 
-integrate f a b n = let h = (b - a) / (fromIntegral n)
-                    in h * (f a + 4 * f a + f b)/3 
+rev :: [Double] -> [Double]
+rev [] = []
+rev (a:x) = rev x ++ [a]
 
--- iterate points of integrate
+driver:: Func -> Double -> Double -> Int -> Double 
+driver f a b n =
+    let h = calcH a b n
+        p = points a b h []
+       -- pr = rev p
+        res = calculateHandler f p n 0 0.0 h
+
+    in res
+
+integrate:: Func -> Double -> Double -> Int -> Double 
+integrate f a b n = 
+    let h = calcH a b n
+    in h * (f a + 4 * f a + f b)/3 
+
+
+calcH:: Double -> Double -> Int -> Double
+calcH a b n = (b - a) / (fromIntegral n)
+
+
+points  ::  Double -> Double -> Double -> [Double] -> [Double]
+points  a b h res
+    | b <= a = (res ++ [b])
+    | otherwise =
+        let i = b - h
+        in points a i h (res ++ [b])
+
+-- points  :: Func -> Double -> Double -> Double -> [Double] -> [Double]
+-- points f a b h res
+--     | b <= a = (res ++ [b])
+--     | otherwise =
+--         let i = a + h--(fromIntegral n)
+--             value = f a
+--         in points f i b h (res ++ [value])
+
+calculateHandler :: Func -> [Double] -> Int -> Int -> Double -> Double -> Double
+calculateHandler f points n iter res h = ((calculate f points n iter res h) * h) / 3
+
+calculate :: Func -> [Double] -> Int -> Int -> Double -> Double -> Double
+calculate f points n iter res h
+    | iter == 0 =
+        let fx = f (points!!iter) 
+        in calculate f points n (iter + 1) (res + fx) h
+
+    | iter >=  n = 
+        let fx = f (points!!iter)          
+        in res + fx 
+
+    | iter `mod` 2 /= 0 = 
+        let fx = f (points!!iter)
+            r = 4 * fx
+        in calculate f points n (iter + 1) (res + r) h
+
+    | iter `mod` 2 == 0 = 
+        let fx = f (points!!iter)
+            r = fx * 2--2 * fx
+        in calculate f points n (iter + 1) (res + r) h
+    
+    | otherwise = res 
+
+
+
--- simpson/main.hs
+++ simpson/main.hs
@@ -1,9 +1,22 @@
 import Simpson
 
 myFunc :: Double -> Double
-myFunc = \x -> x 
+myFunc = \x -> x*x 
+
+printD:: [Double] -> String -> String
+printD [] s = s
+printD listD s = 
+  let h = head listD
+      nl = drop 1 listD
+  in printD nl (s ++ " " ++ (show h))
+
+calcH:: Double -> Double -> Int -> Double
+calcH a b n = (b - a) / (fromIntegral n)
 
 main :: IO ()
-main = do
-  let result = Simpson.integrate myFunc 0.0 8.0 4
-  putStrLn $ show result
+main =
+  let result = Simpson.integrate myFunc 1.0 2.0 100
+      test = Simpson.points  1.0 10.0 (calcH 1.0 10.0 10) []
+      testFull = Simpson.driver myFunc 4.0 10.0 2
+      tester = printD test "" 
+  in putStrLn $ show testFull
